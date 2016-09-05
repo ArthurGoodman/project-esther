6caa5202562624abdb09f6f30553a51c611cfc61
ArthurGoodman <arthur.goodman3@gmail.com>
2016-09-05 11:48:42 +0300

    some object methods

--- esther/source/runtime/object.cpp
+++ esther/source/runtime/object.cpp
@@ -1,14 +1,39 @@
 #include "object.h"
 
+#include "class.h"
+#include "runtime.h"
+#include "utility.h"
+
 Object::Object() {
 }
 
-Object::Object(const std::string &className) {
+Object::Object(Class *objectClass)
+    : objectClass(objectClass) {
+}
+
+Object::Object(const std::string &className)
+    : objectClass((Class *)Runtime::getRootClass(className)) {
 }
 
 Object::~Object() {
 }
 
+Class *Object::getClass() const {
+    return objectClass;
+}
+
+bool Object::hasAttribute(const std::string &name) const {
+    return attributes.find(name) != attributes.end();
+}
+
+Object *Object::getAttribute(const std::string &name) const {
+    return hasAttribute(name) ? attributes.at(name) : 0;
+}
+
+void Object::setAttribute(const std::string &name, Object *value) {
+    attributes[name] = value;
+}
+
 std::string Object::toString() const {
-    return "";
+    return "<" + getClass()->toString() + ":" + Utility::toString((void *)this) + ">";
 }
--- esther/source/runtime/object.h
+++ esther/source/runtime/object.h
@@ -1,13 +1,25 @@
 #pragma once
 
+#include <map>
 #include <string>
 
+class Class;
+
 class Object {
+    Class *objectClass;
+    std::map<std::string, Object *> attributes;
+
 public:
     Object();
+    Object(Class *objectClass);
     Object(const std::string &className);
-
     virtual ~Object();
+
+    Class *getClass() const;
+
+    bool hasAttribute(const std::string &name) const;
+    Object *getAttribute(const std::string &name) const;
+    void setAttribute(const std::string &name, Object *value);
 
     std::string toString() const;
 };
