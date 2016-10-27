4a182d913420ad8fc48634ba5a5b01e585994024
ArthurGoodman <arthur.goodman3@gmail.com>
2016-10-28 00:16:02 +0300

    refactored tests a bit

--- esther/test/source/tests/expressiontest.cpp
+++ esther/test/source/tests/expressiontest.cpp
@@ -2,48 +2,48 @@
 
 ExpressionTest::ExpressionTest()
     : TestSet("expressions") {
-    $("And", [=]() {
-    }).should.be.ok();
+    $("And", [=]() {})
+        .should.be.ok();
 
-    $("AttributeAssignment", [=]() {
-    }).should.be.ok();
+    $("AttributeAssignment", [=]() {})
+        .should.be.ok();
 
-    $("Block", [=]() {
-    }).should.be.ok();
+    $("Block", [=]() {})
+        .should.be.ok();
 
-    $("Call", [=]() {
-    }).should.be.ok();
+    $("Call", [=]() {})
+        .should.be.ok();
 
-    $("Constant", [=]() {
-    }).should.be.ok();
+    $("Constant", [=]() {})
+        .should.be.ok();
 
-    $("Empty", [=]() {
-    }).should.be.ok();
+    $("Empty", [=]() {})
+        .should.be.ok();
 
-    $("Here", [=]() {
-    }).should.be.ok();
+    $("Here", [=]() {})
+        .should.be.ok();
 
-    $("Identifier", [=]() {
-    }).should.be.ok();
+    $("Identifier", [=]() {})
+        .should.be.ok();
 
-    $("If", [=]() {
-    }).should.be.ok();
+    $("If", [=]() {})
+        .should.be.ok();
 
-    $("Literal", [=]() {
-    }).should.be.ok();
+    $("Literal", [=]() {})
+        .should.be.ok();
 
-    $("LocalAssignment", [=]() {
-    }).should.be.ok();
+    $("LocalAssignment", [=]() {})
+        .should.be.ok();
 
-    $("Loop", [=]() {
-    }).should.be.ok();
+    $("Loop", [=]() {})
+        .should.be.ok();
 
-    $("Not", [=]() {
-    }).should.be.ok();
+    $("Not", [=]() {})
+        .should.be.ok();
 
-    $("Or", [=]() {
-    }).should.be.ok();
+    $("Or", [=]() {})
+        .should.be.ok();
 
-    $("Self", [=]() {
-    }).should.be.ok();
+    $("Self", [=]() {})
+        .should.be.ok();
 }
