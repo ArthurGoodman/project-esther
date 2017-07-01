8333943e1e9578c79856b15853eeff9e9be71bbb
ArthurGoodman <arthur.goodman3@gmail.com>
2017-07-02 00:10:50 +0300

    String stuff

--- esther/src/libesther/std_string.c
+++ esther/src/libesther/std_string.c
@@ -1,22 +1,29 @@
 #include "esther/std_string.h"
 
 #include <memory.h>
+#include <stdlib.h>
 
-struct string *string_new(const char *data, size_t length) {
-    struct string *str = malloc(sizeof(struct string));
-    string_init(str, data, length);
+struct string string_new(const char *data, size_t length) {
+    struct string str;
+    string_init(&str, data, length);
     return str;
 }
 
-struct string *string_new_c_str(const char *data) {
-    struct string *str = malloc(sizeof(struct string));
-    string_init_c_str(str, data);
+struct string string_new_c_str(const char *data) {
+    struct string str;
+    string_init_c_str(&str, data);
     return str;
 }
 
-struct string *string_new_empty() {
-    struct string *str = malloc(sizeof(struct string));
-    string_init_empty(str);
+struct string string_new_copy(struct string other) {
+    struct string str;
+    string_init_copy(&str, other);
+    return str;
+}
+
+struct string string_new_empty() {
+    struct string str;
+    string_init_empty(&str);
     return str;
 }
 
@@ -32,19 +39,15 @@ void string_init_c_str(struct string *str, const char *data) {
     str->data = strdup(data);
 }
 
+void string_init_copy(struct string *str, struct string other) {
+    string_init(str, other.data, other.length);
+}
+
 void string_init_empty(struct string *str) {
     str->length = 0;
     str->data = strdup("");
 }
 
-size_t string_length(struct string *str) {
-    return str->length;
-}
-
-const char *string_c_str(struct string *str) {
-    return str->data;
-}
-
-struct string *string_copy(struct string *str) {
-    return string_new(str->data, str->length);
+void string_free(struct string str) {
+    free(str.data);
 }
